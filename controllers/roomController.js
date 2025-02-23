9c82eaca1ee287b545f4852e720da20ab46bf81e
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-23 19:25:37 +0600

    fix(controllers): correct property names in Room Controller

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -2,12 +2,33 @@ const Room = require("../models/roomModel");
 
 // This function is used to add a new room to the database.
 const AddRoom = async (req, res) => {
-  const { title, description } = req.body;
+  const {
+    title,
+    description,
+    host,
+    location,
+    room_details,
+    pricing,
+    availability,
+    amenities,
+    images,
+    safety_features,
+    status,
+  } = req.body;
 
   try {
     const newRoom = new Room({
       title,
       description,
+      host,
+      location,
+      room_details,
+      pricing,
+      availability,
+      amenities,
+      images,
+      safety_features,
+      status,
     });
     await newRoom.save();
     res.status(200).send({
