2234b7596148963ea2573df8e468e88af3f4e559
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-24 22:43:19 +0600

    feat(routes): implement GET endpoints for fetch user's role based data

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { AddUser, loginUser } = require("../controllers/userController");
+const {
+  AddUser,
+  loginUser,
+  getUserRole,
+} = require("../controllers/userController");
 
 router.post("/add-user", AddUser);
 router.post("/login", loginUser);
+router.get("/get-user/:email", getUserRole);
 module.exports = router;
