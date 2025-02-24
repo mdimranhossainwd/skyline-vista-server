98b4f1ab98117e7c8ad99b14df8a3f531d7ccbac
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-24 11:48:42 +0600

    feat(models): add user model for registrations

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: { type: String, required: [true, "Password is required"] },
+    role: { type: String, default: "user" },
+    image: { type: String },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
