3de660aff37e724eca71fba917eee6a8fdccabf1
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-24 11:50:09 +0600

    feat(controllers): add user controller with CRUD operations

---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require("../models/userModel");
+
+const AddUser = async (req, res) => {
+  const { name, email, password, role, image } = req.body;
+  try {
+    const user = new User({
+      name,
+      email,
+      password,
+      role,
+      image,
+    });
+    await user.save();
+    res.status(200).send({
+      message: "User added successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Error in adding user",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { AddUser };
