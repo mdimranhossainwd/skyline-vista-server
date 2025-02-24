a7ef09617fed86b737e13161b901f9aa7c5aafbc
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-24 22:32:49 +0600

    feat(controllers): implement to user role based functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,4 +60,27 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { AddUser, loginUser };
+const getUserRole = async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await User.findOne({ email }).select("role");
+    if (!user) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+    res.status(200).send({
+      message: "User role fetched successfully",
+      data: {
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Error in getting user role",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { AddUser, loginUser, getUserRole };
