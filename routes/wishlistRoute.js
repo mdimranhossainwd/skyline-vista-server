92a0b9516a818c2f17c16ce53719650f0231bb9a
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-25 12:19:10 +0600

    feat(api): implement GET endpoints for fetch all wishlists room data

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   AddToWishlist,
   userWishlistByEmail,
+  getAllWishlist,
   deleteWishlistRoom,
 } = require("../controllers/wishlistController");
 
 router.post("/add-to-wishlist", AddToWishlist);
 router.get("/user-wishlist", userWishlistByEmail);
+router.get("/all-wishlist", getAllWishlist);
 router.delete("/delete-wishlist-room/:id", deleteWishlistRoom);
 
 module.exports = router;
