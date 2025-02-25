00ec18379422cce4f2b94514945c1c2eaec1bdd2
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-25 12:09:01 +0600

    feat(api): implement DELETE endpoints for user wishlists rooms data

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   AddToWishlist,
   userWishlistByEmail,
+  deleteWishlistRoom,
 } = require("../controllers/wishlistController");
 
 router.post("/add-to-wishlist", AddToWishlist);
 router.get("/user-wishlist", userWishlistByEmail);
+router.delete("/delete-wishlist-room/:id", deleteWishlistRoom);
 
 module.exports = router;
