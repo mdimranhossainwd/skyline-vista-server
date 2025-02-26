ee9e110a14d02f4c99899039dc3e6ec024fb86a0
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-26 18:55:45 +0600

    feat(api): implement PUT endpoints for room data by ID

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -4,9 +4,11 @@ const {
   AddRoom,
   GetRooms,
   GetRoomById,
+  UpdateRoom,
 } = require("../controllers/roomController");
 
 router.post("/add-room", AddRoom);
 router.get("/get-rooms", GetRooms);
 router.get("/get-room/:id", GetRoomById);
+router.put("/update-room/:id", UpdateRoom);
 module.exports = router;
