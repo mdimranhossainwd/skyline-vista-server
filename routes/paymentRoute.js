02145ffd862b2402552a72505e2617bbbac856bf
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-02-28 11:54:56 +0600

    feat(api-routes): implement GET endpoints for fetch user's payments data

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   AddToPayment,
   StripeAddPayment,
+  GetRoomPayments,
 } = require("../controllers/paymentController");
 
 router.post("/add-to-payment", AddToPayment);
 router.post("/create-payment-intent", StripeAddPayment);
+router.get("/get-room-payments", GetRoomPayments);
 module.exports = router;
