78de8836c4ec5790c817613f1901077d1e55aea2
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-01 22:39:04 +0600

    fix(controllers): improve amount retrieval issue in AddToPayment function

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Payment = require("../models/paymentModel");
 const Wishlist = require("../models/wishlistModel");
+const Offer = require("../models/offerModel");
 const stripe = require("stripe")(process.env.SKYLINE_VISTA_STRIPE_SECRET_KEY);
 
 const StripeAddPayment = async (req, res) => {
@@ -25,26 +26,51 @@ const StripeAddPayment = async (req, res) => {
 };
 
 const AddToPayment = async (req, res) => {
-  const { room_id } = req.body;
+  const { room_id, offer_id } = req.body;
   try {
-    const wishlist = await Wishlist.findById(room_id);
+    let wishlist = await Wishlist.findById(room_id);
     if (!wishlist) {
       return res.status(404).send({ message: "Room not found" });
     }
-    const payment = new Payment({
-      room: wishlist.room,
-      email: wishlist.email,
-      paymentId: req.body.paymentId,
-      amount: wishlist?.room?.amount,
-      status: req.body.status,
-      date: new Date(),
-    });
-    await payment.save();
-    res.status(200).send({
-      success: true,
-      message: "Payment added successfully",
-      payment,
-    });
+    if (wishlist) {
+      const payment = new Payment({
+        room: wishlist.room,
+        email: wishlist.email,
+        paymentId: req.body.paymentId,
+        amount: wishlist?.room?.amount,
+        status: req.body.status,
+        date: new Date(),
+      });
+      await payment.save();
+      res.status(200).send({
+        success: true,
+        message: "Payment added successfully",
+        payment,
+      });
+    }
+
+    let offer = await Offer.findById(offer_id);
+    if (!offer) {
+      return res.status(404).send({ message: "Offer not found" });
+    }
+    if (offer) {
+      const payment = new Payment({
+        offer: offer.offer,
+        email: offer.email,
+        paymentId: req.body.paymentId,
+        amount: offer?.amount,
+        status: req.body.status,
+        date: new Date(),
+      });
+      await payment.save();
+      res.status(200).send({
+        success: true,
+        message: "Payment added successfully",
+        payment,
+      });
+    }
+
+    res.status(404).send({ message: "Room or Offer not found" });
   } catch (error) {
     console.log("Error", error);
     res.status(400).send({ message: "Error adding payment", error: error });
