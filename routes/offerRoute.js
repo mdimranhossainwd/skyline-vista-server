9bc541bbcfb9763cc4197b396938f5ff27fd1661
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-01 22:04:33 +0600

    feat(api-routes): implement PATCH endpoints for update offer room order status

--- routes/offerRoute.js
+++ routes/offerRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   AddToOffer,
   GetAllOfferRoom,
+  UpdateOfferStatus,
 } = require("../controllers/offerController");
 
 router.post("/add-to-offer", AddToOffer);
 router.get("/get-all-offer", GetAllOfferRoom);
+router.patch("/update-offer-status/:id", UpdateOfferStatus);
 
 module.exports = router;
