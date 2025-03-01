079ef0bb96530d59c3b75ee96a891563a950374b
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-01 18:15:26 +0600

    feat(controllers): add offer controller with CRUD operations

---
+++ controllers/offerController.js
@@ -0,0 +1,31 @@
+const Offer = require("../models/offerModel");
+const Room = require("../models/roomModel");
+
+const AddToOffer = async (req, res) => {
+  const { rooms_id, amount } = req.body;
+  try {
+    const offer = await Room.findById(rooms_id);
+    if (!offer) {
+      return res.status(404).send({ message: "Offer not found" });
+    }
+
+    const newOffer = new Offer({
+      offer,
+      amount,
+    });
+    await newOffer.save();
+    res.status(200).send({
+      success: true,
+      message: "Offer added successfully",
+      data: newOffer,
+    });
+  } catch (err) {
+    console.log("Error:", err),
+      res.status(400).send({
+        message: "Error in adding offer",
+        error: err,
+      });
+  }
+};
+
+module.exports = { AddToOffer };
