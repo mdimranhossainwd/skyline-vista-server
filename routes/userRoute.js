284a5082a8171ef1fd08a64f81b4365e5e505a0d
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-14 15:12:38 +0600

    feat(api-routes): implement GET endpoints fetch all users data for admin

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,12 +8,14 @@ const {
   updateUserRole,
   deleteUser,
   logoutUser,
+  getAllUser,
 } = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddlewares");
 
 router.post("/add-user", AddUser);
 router.post("/login", loginUser);
 router.get("/log-out", logoutUser);
+router.get("/all-users", getAllUser);
 router.post("/jwt", authMiddleware);
 router.get("/get-user/:email", getUserRole);
 router.patch("/update-user/:id", updateUserRole);
