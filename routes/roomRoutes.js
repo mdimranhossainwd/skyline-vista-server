8d44092886a8792b4e7712c0c68ab13b52fe10d1
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-19 08:37:15 +0600

    feat(api-routes): add route to get room by user email

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -7,11 +7,13 @@ const {
   UpdateRoomStatus,
   UpdateRoom,
   DeleteRoom,
+  GetRoomByUserEmail,
 } = require("../controllers/roomController");
 
 router.post("/add-room", AddRoom);
 router.get("/get-rooms", GetRooms);
 router.get("/get-room/:id", GetRoomById);
+router.get("/get-room-by-email", GetRoomByUserEmail);
 router.put("/update-room/:id", UpdateRoom);
 router.patch("/update-room-status/:id", UpdateRoomStatus);
 router.delete("/delete-room/:id", DeleteRoom);
