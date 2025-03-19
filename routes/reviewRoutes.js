ac76485ff073e8387cec57410ea9a3740cbc77d0
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-20 00:06:00 +0600

    feat(api-routes): add DELETE endpoint for removing reviews by ID

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { AddReview, GetReviews } = require("../controllers/reviewController");
+const {
+  AddReview,
+  GetReviews,
+  DeleteReview,
+} = require("../controllers/reviewController");
 
 router.post("/add-review", AddReview);
 router.get("/get-reviews", GetReviews);
+router.delete("/delete-review/:id", DeleteReview);
 
 module.exports = router;
