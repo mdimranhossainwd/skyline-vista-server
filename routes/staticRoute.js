1a8572edbdb22540c45406a408396539f01491de
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-03-21 15:21:33 +0600

    feat(api-route): add route for fetching user specific email statistics data

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getStatics,
   getAgentStatics,
+  getUserStatics,
 } = require("../controllers/staticController");
 
 router.get("/get-statics", getStatics);
 router.get("/get-agent-statics", getAgentStatics);
+router.get("/get-user-statics", getUserStatics);
 
 module.exports = router;
